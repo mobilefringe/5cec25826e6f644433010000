a9f5084d541d4f8b87940630fd0377a5cdb77a5b
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 10:37:45 -0400

    Central Park Plaza 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(204, 0, 27, 0.8), rgba(244, 92, 0, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(204, 0, 27)' }">
+    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(204, 0, 27, 0.8), rgba(204, 0, 27, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(204, 0, 27)' }">
         <div class="main_container">
             <div class="page_container">
                 <h2 v-html="pageName"></h2>
