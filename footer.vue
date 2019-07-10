335ac984707da1638965d7af6b617ac024babc73
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-10 12:47:27 -0400

    Central Park Plaza 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -55,12 +55,8 @@
             data: function data() {
                 return {
                     dataLoaded: false,
-                    siteInfo: site,
-                    newsletter_email: ""
+                    siteInfo: site
                 }
-            },
-            mounted () {
-                window.MktoForms2.loadForm("//app-sj03.marketo.com", "561-LJY-710", 3300);
             },
             created () {
                 this.dataLoaded = true;
@@ -68,21 +64,13 @@
             computed: {
                 ...Vuex.mapGetters([
                     'property',
-                    'timezone',
-                    'findRepoByName'
+                    'timezone'
                 ]),
                 copyright_year() {
                     return moment().year();
                 },
                 getPropertyAddress() {
                     return this.property.address1 + ' ' + this.property.city + ' ' + this.property.country + ' ' + this.property.province_state
-                }
-            },
-            methods: {
-                newsletterRoute() {
-                    this.show_menu = false;
-                    this.$router.push("/newsletter?email=" + this.newsletter_email);
-                    this.newsletter_email = "";
                 }
             }
         });
